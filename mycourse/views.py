6f2290c3843a3cda63c9260504eb47b5c09514cc
steveGau <95110981+steveGau@users.noreply.github.com>
2023-02-12 22:38:07 -0500

    Add files via upload

--- mycourse/views.py
+++ mycourse/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render
-from mycourse.models import *
+# from mycourse.models import *
+from .models import Dept, Course, Student
 
 def home(request):
-    data1 = mycourse.Dept.objects.all()
+    data1 = Dept.objects.all()
+    data2 = Course.objects.all()
+    data3 = Student.objects.all()
     for dept in data1:
         print(dept.dept_id, dept.name)
 
-    data2 = mycourse.Course.objects.all()
     for course in data2:
         print(course.course_id, course.name)
+        
 
-    data3 = mycourse.Student.objects.all()
     for student in data3:
         print(student.user, student.dept_id, student.prn, student.name, student.DOB)
 
     return render(request, 'index.html', {'query1': data1,'query2': data2,'query3': data3})
+
+'''
+ProgrammingError at /
+relation "mycourse_dept" does not exist
+LINE 1: ...urse_dept"."dept_id", "mycourse_dept"."name" FROM "mycourse_...
+                                                             ^
+Request Method:	GET
+Request URL:	https://render-django-postgresql-html-foreignkey-qka2.onrender.com/
+Django Version:	4.1.6
+Exception Type:	ProgrammingError
+Exception Value:	
+relation "mycourse_dept" does not exist
+LINE 1: ...urse_dept"."dept_id", "mycourse_dept"."name" FROM "mycourse_...
+                                                             ^
+Exception Location:	/opt/render/project/src/.venv/lib/python3.10/site-packages/django/db/backends/utils.py, line 89, in _execute
+Raised during:	mycourse.views.home
+'''
